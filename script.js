8ee7a1804b0e1d218cff433614e42610319566e6
iCaeeden <brandoncadensilao@gmail.com>
2024-11-15 14:03:54 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -80,6 +80,31 @@ if (cards.length > 0) {
     setupCardSwipe(cards[currentIndex]);
 }
 
+function toggleFullscreen() {
+    if (!document.fullscreenElement) {
+        // Attempt to make the document fullscreen
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+            document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+            document.documentElement.msRequestFullscreen();
+        }
+    } else {
+        // If already in fullscreen, exit fullscreen
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) { // Firefox
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { // IE/Edge
+            document.msExitFullscreen();
+        }
+    }
+}
 
 
 
